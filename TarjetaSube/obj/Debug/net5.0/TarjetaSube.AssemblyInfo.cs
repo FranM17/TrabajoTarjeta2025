3bbc004d8ff0b596f6d81bfca363ada47dc54dab
Franco Mondino <franmondi17@gmail.com>
2025-10-21 16:12:16 -0300

    Eliminacion de carpetas innecesarias

--- TarjetaSube/obj/Debug/net5.0/TarjetaSube.AssemblyInfo.cs
+++ TarjetaSube/obj/Debug/net5.0/TarjetaSube.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TarjetaSube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0176cef10cdf7cc3491f732b1bc1c4c5434d32e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TarjetaSube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TarjetaSube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
