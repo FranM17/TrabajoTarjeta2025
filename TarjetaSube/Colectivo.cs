0b04fa53112e98743f9b8daacd70031aa056923d
Franco Rubin <rubinfranco07@gmail.com>
2025-10-21 16:36:13 -0300

    actualizo colectivo

--- TarjetaSube/Colectivo.cs
+++ TarjetaSube/Colectivo.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace TarjetaSube
 {
-    internal class Colectivo
+    public class Colectivo
     {
+        private const decimal TARIFA_BASICA = 1580m;
+        private string linea;
+
+        public string Linea
+        {
+            get { return linea; }
+        }
+
+        public Colectivo(string linea)
+        {
+            this.linea = linea;
+        }
+
+        public Boleto? PagarCon(Tarjeta tarjeta)
+        {
+            if (tarjeta.DescontarSaldo(TARIFA_BASICA))
+            {
+                return new Boleto(TARIFA_BASICA, tarjeta.Saldo, this.linea);
+            }
+            return null;
+        }
     }
 }
